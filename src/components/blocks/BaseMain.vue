a723469292ac71f91eba9d5a701857972a280f46
Greg1928 <gregoriosortino@protonmail.com>
2022-06-09 19:45:50 +0200

    added dynamic flags

--- src/components/blocks/BaseMain.vue
+++ src/components/blocks/BaseMain.vue
@@ -6,7 +6,7 @@
       <li v-for="(film, i) in films" :key="film.id">
         <h1>Titolo: {{film.title}}</h1>
         <h2>Titolo Originale: {{film.original_title}}</h2>
-        <h3>{{dynamicFlags(i)}}</h3>
+        <h3>Lingua: <img :src="dynamicFlags(i)"/></h3>
         <h4>Voto: {{film.vote_average}}</h4>
         <hr>
       </li>
@@ -16,6 +16,11 @@
 
 <script>
 import axios from 'axios'
+import imageIt from '../../assets/img/220px-Flag_of_Italy_(Pantone,_2003–2006).svg.png'
+import imageGb from '../../assets/img/Eng.png'
+import imageEs from '../../assets/img/Flag_of_Spain.svg.png'
+import imageFr from '../../assets/img/Flag_of_France.png'
+import imageRest from '../../assets/img/mundo-comunicacion-red_42634-20.webp.png'
 
 export default {
   name: 'BaseMain',
@@ -23,7 +28,6 @@ export default {
         return{
             films: [],
             InputText: '',
-            prova: 1
         }
     },
     methods:{
@@ -32,7 +36,7 @@ export default {
             params:{
                 api_key: '8c5b607d815956781cffe0cfa80918d4',
                 query: this.InputText,
-                language: 'it-IT'
+                // language: 'it-IT'
             }
         }).then((response) =>{
             this.films = response.data.results
@@ -42,8 +46,16 @@ export default {
       },
     dynamicFlags(i){
       if(this.films[i].original_language === 'it'){
-        return this.prova
-      }
+        return imageIt
+      }else if(this.films[i].original_language === 'en'){
+        return imageGb
+      }else if(this.films[i].original_language === 'es'){
+        return imageEs
+    }else if(this.films[i].original_language === 'fr'){
+        return imageFr
+    }else{
+      return imageRest
+    }
     }
 }
 }
@@ -51,5 +63,8 @@ export default {
 
 
 <style scoped lang="scss">
-
+img{
+  max-width: 30px;
+  vertical-align: middle;
+}
 </style>
