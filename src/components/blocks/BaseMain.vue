a978d11595fef4717e16ddc2c935209a7361e9bf
Greg1928 <gregoriosortino@protonmail.com>
2022-06-09 17:56:57 +0200

    added milestone 1

---
+++ src/components/blocks/BaseMain.vue
@@ -0,0 +1,48 @@
+<template>
+  <main>
+    <input type="text" v-model="InputText">
+    <button @click="filterFilms()">Search</button>
+    <ul>
+      <li v-for="film in films" :key="film.id">
+        <h1>Titolo: {{film.title}}</h1>
+        <h2>Titolo Originale: {{film.original_title}}</h2>
+        <h3>Lingua: {{film.original_language}}</h3>
+        <h4>Popolarità: {{film.popularity}}</h4>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'BaseMain',
+   data(){
+        return{
+            films: [],
+            InputText: ''
+        }
+    },
+    methods:{
+      filterFilms(){
+        axios.get('https://api.themoviedb.org/3/search/movie', {
+            params:{
+                api_key: '8c5b607d815956781cffe0cfa80918d4',
+                query: this.InputText,
+                language: 'it-IT'
+            }
+        }).then((response) =>{
+            this.films = response.data.results
+        }).catch((err) => {
+            console.log(err);
+        })
+      }
+    }
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
