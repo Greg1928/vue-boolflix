a3399e933a3f0fca88d833237fdab51015acd2f1
Greg1928 <gregoriosortino@protonmail.com>
2022-06-10 13:16:02 +0200

    added milestone 2

--- src/components/blocks/BaseMain.vue
+++ src/components/blocks/BaseMain.vue
@@ -1,16 +1,27 @@
 <template>
   <main>
-    <input type="text" placeholder="Cerca film o serie Tv" @keyup="filterFilms()" v-model="InputText">
-    <button @click="filterFilms()">Search</button>
+    <input type="text" placeholder="Cerca film o serie Tv" v-model="InputText">
+    <button @click="filterTv(), filterFilms()">Search</button>
     <SectionTrending :class="{show : InputText === ''}"/>
     <div :class="{none : InputText === ''}">
-      <h1>I tuoi risultati</h1>
+      <h1>I tuoi risultati nella categoria Film</h1>
       <ol>
         <li v-for="(film, i) in films" :key="film.id">
           <h1>Titolo: {{film.title}}</h1>
           <h2>Titolo Originale: {{film.original_title}}</h2>
-          <h3>Lingua: <img :src="dynamicFlags(i)"/></h3>
+          <h3>Lingua: <img :src="dynamicFlagsFilm(i)"/></h3>
           <h4>Voto: {{film.vote_average}}</h4>
+        </li>
+      </ol>
+    </div>
+     <div :class="{none : InputText === ''}">
+      <h1>I tuoi risultati nella categoria TV</h1>
+      <ol>
+        <li v-for="(tv, i) in Tv" :key="tv.id">
+          <h1>Titolo: {{tv.name}}</h1>
+          <h2>Titolo Originale: {{tv.original_title}}</h2>
+          <h3>Lingua: <img :src="dynamicFlagsTv(i)"/></h3>
+          <h4>Voto: {{tv.vote_average}}</h4>
         </li>
       </ol>
     </div>
@@ -32,6 +43,7 @@ export default {
    data(){
         return{
             films: [],
+            Tv: [],
             InputText: '',
 
         }
@@ -50,14 +62,41 @@ export default {
             console.log(err);
         })
       },
-    dynamicFlags(i){
-      if(this.films[i].original_language === 'it'){
+      filterTv(){
+        axios.get('https://api.themoviedb.org/3/search/tv', {
+            params:{
+                api_key: '8c5b607d815956781cffe0cfa80918d4',
+                query: this.InputText,
+                // language: 'it-IT'
+            }
+        }).then((response) =>{
+            this.Tv = response.data.results
+            console.log(this.Tv);  
+        }).catch((err) => {
+            console.log(err);
+        })
+      },
+    dynamicFlagsFilm(i){
+      if(this.films[i].original_language === 'it' ){
         return imageIt
       }else if(this.films[i].original_language === 'en'){
         return imageGb
       }else if(this.films[i].original_language === 'es'){
         return imageEs
     }else if(this.films[i].original_language === 'fr'){
+        return imageFr
+    }else{
+      return imageRest
+    }
+    },
+    dynamicFlagsTv(i){
+      if(this.Tv[i].original_language === 'it' ){
+        return imageIt
+      }else if(this.Tv[i].original_language === 'en'){
+        return imageGb
+      }else if(this.Tv[i].original_language === 'es'){
+        return imageEs
+    }else if(this.Tv[i].original_language === 'fr'){
         return imageFr
     }else{
       return imageRest
